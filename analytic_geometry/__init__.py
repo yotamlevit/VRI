e2e6215cb19e473a92e36d377bcb36da8ab9befb
Yotam Levit <yotam.levit@gmail.com>
2021-11-26 10:59:14 +0200

    Fix __init__

--- analytic_geometry/__init__.py
+++ analytic_geometry/__init__.py
@@ -1,13 +1,3 @@
 # -*- coding: utf-8 -*-
 
 __all__ = ['Point', 'Vector', 'StraightLine']
-
-def main():
-    """
-    Add Documentation here
-    """
-    pass  # Replace Pass with Your Code
-
-
-if __name__ == '__main__':
-    main()
